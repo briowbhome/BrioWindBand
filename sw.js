2155162538ae252baf56453ceb497d017e32070d
DESKTOP-NGIOCMP\MSI <harry94014@gmail.com>
2026-08-15 18:08:21 +0800

    新增PWA檔案

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_VERSION = 'brio-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './login.html',
+  './checkin.html',
+  './admin.html',
+  './event-admin.html',
+  './checkin-admin.html',
+  './auth-guard.js',
+  './auth-service.js',
+  './firebase-init.js',
+  './admin-nav.js',
+  './manifest.json',
+  './assets/BrioLogo.jpg',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== 'GET' || url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
